4c0ce58c08e9bde63a1b815b6e1b1a5475cc8fe5
kelvin-de-oliveira <kelvin.oliveira.0110@gmail.com>
2026-06-24 20:51:54 -0300

    bugfix: caminho de importacao config/logging

--- apigateway/main.py
+++ apigateway/main.py
@@ -4,8 +4,8 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from core.config import settings
-from core.logging import setup_logging
+from app.core.config import settings
+from app.core.logging import setup_logging
 
 
 setup_logging()
